9d58c0beb1c6a7929043685e855c6fddbbf49194
RogerioSouza-f <souzarogerio33freitas@gmail.com>
2025-10-06 00:58:56 -0300

    Add Model

---
+++ ListaDeTarefas/src/main/java/github/ioRogerioSouza_f/ListaDeTarefas/model/Tarefa.java
@@ -0,0 +1,30 @@
+package github.ioRogerioSouza_f.ListaDeTarefas.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Tarefa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa() {}
+
+    public Tarefa(String descricao, boolean concluida) {
+        this.descricao = descricao;
+        this.concluida = concluida;
+    }
+
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public boolean isConcluida() { return concluida; }
+    public void setConcluida(boolean concluida) { this.concluida = concluida; }
+}
