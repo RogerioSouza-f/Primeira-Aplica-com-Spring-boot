8631716677fa651678c0cd090c87f3dc258f2bdb
RogerioSouza-f <souzarogerio33freitas@gmail.com>
2025-10-06 00:56:39 -0300

    first commit

---
+++ ListaDeTarefas/src/main/java/github/ioRogerioSouza_f/ListaDeTarefas/ListaDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package github.ioRogerioSouza_f.ListaDeTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeTarefasApplication.class, args);
+	}
+
+}
---
+++ ListaDeTarefas/src/test/java/github/ioRogerioSouza_f/ListaDeTarefas/ListaDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package github.ioRogerioSouza_f.ListaDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
